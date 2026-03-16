2d632d873d88c6bc258561e7038780c430ff3abd
Sathish27mca <sadheshkumar.p@cognizant.com>
2026-03-16 16:24:43 +0530

    logincomponent added

---
+++ src/app/controller/logincomponent/logincomponent.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logincomponent',
+  imports: [],
+  templateUrl: './logincomponent.component.html',
+  styleUrl: './logincomponent.component.css'
+})
+export class LogincomponentComponent {
+
+}
